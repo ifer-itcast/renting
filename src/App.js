a5c6dc19fadf923269f5244a8853ff3ea6510012
ifer-itcast <yangkangkang@itcast.cn>
2019-12-18 00:11:26 +0800

    antd-mobile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Button } from 'antd-mobile';
 
 function App() {
-	return <div className="App">hello world</div>;
+	return <div className="App">hello world <Button>Button</Button></div>;
 }
 
 export default App;
