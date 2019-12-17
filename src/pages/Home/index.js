71cf41f3204227a9ec3a5b94cfbdcf862e725d81
ifer-itcast <yangkangkang@itcast.cn>
2019-12-18 00:40:32 +0800

    嵌套路由

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import News from '../News';
 
 export default class Home extends React.Component {
-    render () {
-        return <div>
-            首页
-        </div>
-    }
+	render() {
+		return (
+			<div>
+				首页
+				<Route path="/home/news" component={News} />
+			</div>
+		);
+	}
 }
