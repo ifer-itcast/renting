9e8e1f2761c3a5fc630339c91fb47b324a30665b
ifer-itcast <yangkangkang@itcast.cn>
2019-12-18 13:27:52 +0800

    使用轮播图组件

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Carousel } from 'antd-mobile';
 
 export default class Index extends React.Component {
-    render () {
-        return <div>
-            首页
-        </div>
-    }
+	state = {
+		data: ['1', '2', '3']
+	};
+	componentDidMount() {
+		// simulate img loading
+		setTimeout(() => {
+			this.setState({
+				data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI']
+			});
+		}, 100);
+	}
+	render() {
+		return (
+			<div className="index">
+				<Carousel autoplay infinite>
+					{this.state.data.map(val =>
+						<a
+							key={val}
+							href="http://www.alipay.com"
+							style={{ display: 'inline-block', width: '100%', height: 212 }}
+						>
+							<img
+								src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+								alt=""
+								style={{ width: '100%', verticalAlign: 'top' }}
+							/>
+						</a>
+					)}
+				</Carousel>
+			</div>
+		);
+	}
 }
