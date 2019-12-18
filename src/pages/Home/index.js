46b0f9128ef42da65dcb6c8ca92591a89cd5f849
ifer-itcast <yangkangkang@itcast.cn>
2019-12-18 12:18:35 +0800

    TabBar代码重构

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,82 +7,68 @@ import HouseList from '../HouseList';
 import Profile from '../Profile';
 import './index.css';
 
+// 不变的数据，没必要存成状态
+const tabItems = [
+	{
+		title: '首页',
+		icon: 'icon-ind',
+		path: '/home/index'
+	},
+	{
+		title: '找房',
+		icon: 'icon-findHouse',
+		path: '/home/list'
+	},
+	{
+		title: '资讯',
+		icon: 'icon-infom',
+		path: '/home/news'
+	},
+	{
+		title: '我的',
+		icon: 'icon-my',
+		path: '/home/profile'
+	}
+];
+
 export default class Home extends React.Component {
 	state = {
 		selectedTab: this.props.location.pathname
 	};
+	renderTabBarItem() {
+		return tabItems.map(item => {
+			return (
+				<TabBar.Item
+					title={item.title}
+					key={item.title}
+					icon={<i className={`iconfont ${item.icon}`} />}
+					selectedIcon={<i className={`iconfont ${item.icon}`} />}
+					selected={this.state.selectedTab === item.path}
+					onPress={() => {
+						this.setState({
+							selectedTab: item.path
+						});
+						this.props.history.push(item.path);
+					}}
+				/>
+			);
+		});
+	}
 	render() {
 		return (
 			<div className="home">
-                {/* 子路由 */}
-                {/* 首页 */}
-                <Route path="/home/index" component={Index}/>
-                {/* 找房 */}
-                <Route path="/home/list" component={HouseList}/>
-                {/* 资讯 */}
-                <Route path="/home/news" component={News}/>
-                {/* 我的 */}
-                <Route path="/home/profile" component={Profile}/>
+				{/* 子路由 */}
+				{/* 首页 */}
+				<Route path="/home/index" component={Index} />
+				{/* 找房 */}
+				<Route path="/home/list" component={HouseList} />
+				{/* 资讯 */}
+				<Route path="/home/news" component={News} />
+				{/* 我的 */}
+				<Route path="/home/profile" component={Profile} />
 
 				<TabBar tintColor="#21b97a" barTintColor="white" noRenderContent>
-					<TabBar.Item
-						title="首页"
-						key="Life"
-						icon={<i className="iconfont icon-ind" />}
-						selectedIcon={<i className="iconfont icon-ind" />}
-						selected={this.state.selectedTab === '/home/index'}
-						onPress={() => {
-							this.setState({
-								selectedTab: '/home/index'
-							});
-                            this.props.history.push('/home/index');
-						}}
-						data-seed="logId"
-					>
-					</TabBar.Item>
-					<TabBar.Item
-						icon={<i className="iconfont icon-findHouse" />}
-						selectedIcon={<i className="iconfont icon-findHouse" />}
-						title="找房"
-						key="Koubei"
-						selected={this.state.selectedTab === '/home/list'}
-						onPress={() => {
-							this.setState({
-								selectedTab: '/home/list'
-							});
-                            this.props.history.push('/home/list');
-						}}
-						data-seed="logId1"
-					>
-					</TabBar.Item>
-					<TabBar.Item
-						icon={<i className="iconfont icon-infom" />}
-						selectedIcon={<i className="iconfont icon-infom" />}
-						title="资讯"
-						key="Friend"
-						selected={this.state.selectedTab === '/home/news'}
-						onPress={() => {
-							this.setState({
-								selectedTab: '/home/news'
-							});
-                            this.props.history.push('/home/news');
-						}}
-					>
-					</TabBar.Item>
-					<TabBar.Item
-						icon={<i className="iconfont icon-my" />}
-						selectedIcon={<i className="iconfont icon-my" />}
-						title="我的"
-						key="my"
-						selected={this.state.selectedTab === '/home/profile'}
-						onPress={() => {
-							this.setState({
-								selectedTab: '/home/profile'
-							});
-                            this.props.history.push('/home/profile');
-						}}
-					>
-					</TabBar.Item>
+					{this.renderTabBarItem()}
 				</TabBar>
 			</div>
 		);
