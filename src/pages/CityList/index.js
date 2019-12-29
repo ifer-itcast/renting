f7a759ffdcd643efe0d7f6da4dbf471ebaf39201
ifer-itcast <yangkangkang@itcast.cn>
2019-12-29 13:26:38 +0800

    城市选择顶部导航栏

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { NavBar, Icon } from 'antd-mobile';
+import './index.scss';
 
 export default class CityList extends React.Component {
-    render () {
-        return <div>
-            城市选中
-        </div>
-    }
+	render() {
+		return (
+			<div className="citylist">
+				<NavBar
+                    className="navbar"
+					mode="light"
+					icon={<i className="iconfont icon-back" />}
+					onLeftClick={() => this.props.history.go(-1)}
+				>
+					城市选择
+				</NavBar>
+			</div>
+		);
+	}
 }
