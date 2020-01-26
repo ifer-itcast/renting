4ca33b788943c68687b3a59bc734f95446bf7a0a
ifer-itcast <yangkangkang@itcast.cn>
2020-01-26 18:54:14 +0800

    房屋详情模块结构搭建

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Home from './pages/Home';
 import CityList from './pages/CityList';
 import Map from './pages/Map';
+import HouseDetail from './pages/HouseDetail';
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
 				{/* 配置路由 */}
-				<Route path="/" render={() => <Redirect to="/home"/>}/>
+				<Route path="/" exact render={() => <Redirect to="/home"/>}/>
 				<Route path="/home" component={Home} />
 				<Route path="/citylist" component={CityList} />
 				<Route path="/map" component={Map} />
+				{/* 房源详情的路由规则 */}
+				<Route path="/detail/:id" component={HouseDetail}/>
 			</div>
 		</Router>
 	);
