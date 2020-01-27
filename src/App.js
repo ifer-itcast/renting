df17c3d60a21706c16200dc0d6226b6a87118017
ifer-itcast <yangkangkang@itcast.cn>
2020-01-27 17:28:14 +0800

    配置房源模块相关的路由规则

--- src/App.js
+++ src/App.js
@@ -10,19 +10,33 @@ import HouseDetail from './pages/HouseDetail';
 import Login from './pages/Login';
 import Registe from './pages/Registe';
 
+// 房源发布
+import Rent from './pages/Rent';
+import RentAdd from './pages/Rent/Add';
+import RentSearch from './pages/Rent/Search';
+
+// 路由访问控制组件
+import AuthRoute from './components/AuthRoute';
+
 function App() {
 	return (
 		<Router>
 			<div className="App">
 				{/* 配置路由 */}
-				<Route path="/" exact render={() => <Redirect to="/home"/>}/>
+				<Route path="/" exact render={() => <Redirect to="/home" />} />
 				<Route path="/home" component={Home} />
 				<Route path="/citylist" component={CityList} />
 				<Route path="/map" component={Map} />
 				{/* 房源详情的路由规则 */}
-				<Route path="/detail/:id" component={HouseDetail}/>
+				<Route path="/detail/:id" component={HouseDetail} />
 				<Route path="/login" component={Login} />
 				<Route path="/registe" component={Registe} />
+
+				{/* 配置登录后，才能访问的页面 */}
+				<AuthRoute exact path="/rent" component={Rent} />
+				{/* 当 /rent/add 需要使用 /rent 的内容时，才需要写 /rent 里面 */}
+				<AuthRoute path="/rent/add" component={RentAdd} />
+				<AuthRoute path="/rent/search" component={RentSearch} />
 			</div>
 		</Router>
 	);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,28 +1,29 @@
 import axios from 'axios';
 
 export const getCurrentCity = () => {
-    // 判断 localStorage 中是否有
-    const localCity = JSON.parse(localStorage.getItem('hkzf_city'));
-    if (!localCity) {
-        return new Promise((resolve, reject) => {
-            // 如果没有，使用首页中获取定位城市的代码来获取，并且存储到本地，然后返回该城市数据
-            const curCity = new window.BMap.LocalCity();
-            curCity.get(async res => {
-                try {
-                    const result = await axios.get(`http://localhost:8080/area/info?name=${res.name}`);
-                    localStorage.setItem('hkzf_city', JSON.stringify(result.data.body));
-                    resolve(result.data.body);
-                } catch (e) {
-                    reject(e);
-                }
-            });
-        });
-    }
+	// 判断 localStorage 中是否有
+	const localCity = JSON.parse(localStorage.getItem('hkzf_city'));
+	if (!localCity) {
+		return new Promise((resolve, reject) => {
+			// 如果没有，使用首页中获取定位城市的代码来获取，并且存储到本地，然后返回该城市数据
+			const curCity = new window.BMap.LocalCity();
+			curCity.get(async res => {
+				try {
+					const result = await axios.get(`http://localhost:8080/area/info?name=${res.name}`);
+					localStorage.setItem('hkzf_city', JSON.stringify(result.data.body));
+					resolve(result.data.body);
+				} catch (e) {
+					reject(e);
+				}
+			});
+		});
+	}
 
-    // 为了返回值的统一，此处也使用 Promise
-    return Promise.resolve(localCity);
+	// 为了返回值的统一，此处也使用 Promise
+	return Promise.resolve(localCity);
 };
 
 export { API } from './api';
 export { BASE_URL } from './url';
 export * from './auth';
+export * from './city';
